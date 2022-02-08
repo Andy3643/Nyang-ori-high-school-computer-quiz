11e97be5ae4c09e0387d7669bc43bf5580185d93
Andy3643 <joshandy2021@gmail.com>
2022-02-08 08:31:25 +0300

    feat:add function to check correct scores

--- script.js
+++ script.js
@@ -7,60 +7,58 @@ function quizarea(){
 }
 
 
-// function scores(){
-//         var showDiv = document.getElementById("score");
-//         showDiv.style.display="block";
+function scores(){
+       var showDiv = document.getElementById("score");
+        showDiv.style.display="block";
      
     
-//     var totalScore = 0
-//     var correctAnswer1 = document.javaquiz.quiz1.value;
-//         if (correctAnswer1=="B"){totalScore++};
+     var totalScore = 0
+     var correctAnswer1 = document.javaquiz.quiz1.value;
+        if (correctAnswer1=="B"){totalScore++};
       
 
-//     var correctAnswer2 = document.javaquiz.quiz2.value;
-//         if (correctAnswer2=="D"){totalScore++};
+    var correctAnswer2 = document.javaquiz.quiz2.value;
+        if (correctAnswer2=="D"){totalScore++};
       
 
-//     var correctAnswer3 = document.javaquiz.quiz3.value;
-//         if (correctAnswer3=="A"){totalScore++};
+    var correctAnswer3 = document.javaquiz.quiz3.value;         if (correctAnswer3=="A"){totalScore++};
       
 
-//     var correctAnswer4 = document.javaquiz.quiz4.value;
-//         if (correctAnswer4=="B"){totalScore++};
+    var correctAnswer4 = document.javaquiz.quiz4.value;
+        if (correctAnswer4=="B"){totalScore++};
       
 
-//     var correctAnswer5 = document.javaquiz.quiz5.value;
-//         if (correctAnswer5=="D"){totalScore++};
+    var correctAnswer5 = document.javaquiz.quiz5.value;
+        if (correctAnswer5=="D"){totalScore++};
       
 
-//     var correctAnswer6 = document.javaquiz.quiz6.value;
-//         if (correctAnswer6=="C"){totalScore++};
+    var correctAnswer6 = document.javaquiz.quiz6.value;
+             if (correctAnswer6=="C"){totalScore++};
       
 
-//     var correctAnswer7 = document.javaquiz.quiz7.value;
-//         if (correctAnswer7=="C"){totalScore++};
+    var correctAnswer7 = document.javaquiz.quiz7.value;
+        if (correctAnswer7=="C"){totalScore++};
       
 
-//     var correctAnswer8 = document.javaquiz.quiz8.value;
-//         if (correctAnswer8=="A"){totalScore++};
-      
-
-//     var correctAnswer9 = document.javaquiz.quiz9.value;
-//         if (correctAnswer9=="D"){totalScore++};
+    var correctAnswer8 = document.javaquiz.quiz8.value;
+        if (correctAnswer8=="A"){totalScore++};
+    
+    var correctAnswer9 = document.javaquiz.quiz9.value;
+        if (correctAnswer9=="D"){totalScore++};
      
 
-//     var correctAnswer10 = document.javaquiz.quiz10.value;
-//         if (correctAnswer10=="B"){totalScore++};
+    var correctAnswer10 = document.javaquiz.quiz10.value;
+       if (correctAnswer10=="B"){totalScore++};
       
 
-//         var showDiv = document.getElementById("quizarea");
-//         showDiv.style.display="none";
+   var showDiv = document.getElementById("quizarea");
+         showDiv.style.display="none";
 
-//     var showMarks= document.getElementById("score");
-//     showMarks.textContent=totalScore;
-//     if (totalScore<=5){
-//         showMarks.innerText="your score is " + totalScore +" ,this is poor. Kindly redo the test!";
-//     }
+    var showMarks= document.getElementById("score");
+    showMarks.textContent=totalScore;
+       if (totalScore<=5){
+        showMarks.innerText="your score is " + totalScore +" ,this is poor. Kindly redo the test!";
+     }
 //     else if (totalScore>5 && totalScore<8){
 //         showMarks.innerText="your score is " + totalScore +" ,you have performed avaragely well!";
 //     }
